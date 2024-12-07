a592a378904392d6457d51d92cbeaca6fba46ba0
JunsangYooUSC <junsang@usc.edu>
2024-12-07 13:59:22 -0800

    commit

--- src/host_utils.h
+++ src/host_utils.h
@@ -20,6 +20,7 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <random>
 // Include project headers
 #include "conv_config.h"
 
@@ -63,10 +64,12 @@ void print_data_types() {
 
 // gen_rand: generate random array
 template<typename DTYPE, unsigned int LEN>
-void gen_rand(DTYPE arr[LEN], float min_val, float max_val) {
+void gen_rand(DTYPE arr[LEN], float min_val, float max_val, unsigned int seed=1) {
+	std::mt19937 rng(seed);
+	std::uniform_real_distribution<float> dist(min_val, max_val);
 	for (int i = 0; i < LEN; i++) {
 		// Generate random float within range
-		float rand_val = min_val + static_cast<float>(std::rand()) / RAND_MAX * (max_val - min_val);
+		float rand_val = dist(rng);
 		// Convert to fixed point
 		arr[i] = static_cast<DTYPE>(rand_val);
 	}
