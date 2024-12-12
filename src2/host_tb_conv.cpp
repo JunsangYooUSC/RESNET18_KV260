aeb405d49ab2c30658e8120e8cb1bfb57a2d0b1c
JunsangYooUSC <junsang@usc.edu>
2024-12-11 23:17:38 -0800

    commit

--- src2/host_tb_conv.cpp
+++ src2/host_tb_conv.cpp
@@ -138,6 +138,7 @@ int main(){
 	// compare with golden result
 	compare_result<DTYPE_ACT, float, TOTAL_OUT_LEN>(out_act_host, out_act_host_float, 2.0/(1<<(W_ACT-I_ACT)));
 
+	kernel_func(in_act_host, in_fil_host, out_act_host);
 	// print some results
 	// std::cout << "in_act_host[0]:" << in_act_host[0] << std::endl;
 	// std::cout << "in_act_host_float[0]:" << in_act_host_float[0] << std::endl;
