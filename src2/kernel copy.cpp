b7c8deed4727641cd5fb601957e602d321cb6417
JunsangYooUSC <junsang@usc.edu>
2024-12-12 22:22:34 -0800

    commit

--- src2/kernel copy.cpp
+++ src2/kernel copy.cpp
@@ -27,6 +27,145 @@
 #include "dummy_func.h"
 
 // BUF2PE
+void BUF2PE(
+    // DTYPE_ACT *input_buffer, 
+    DTYPE_ACT input_buffer[2][POY+PAD*2][POX+PAD*2],
+    hls::stream<DTYPE_ACT> mac_in_fifo_arr[POY][POX],
+    unsigned int nkx,
+    unsigned int nky,
+    unsigned int total_loops,       // 
+    unsigned int db_idx     // double buffering index) 
+) {
+    // register R* in fig13 of Optimizing_the_Convolution_Operation_to_Accelerate_Deep_Neural_Networks_on_FPGA
+    DTYPE_ACT buf2pe_reg[POY][POX+1];
+    // #pragma HLS BIND_STORAGE variable=buf2pe_reg type=register
+    // fifo in fig13 of Optimizing_the_Convolution_Operation_to_Accelerate_Deep_Neural_Networks_on_FPGA
+    hls::stream<DTYPE_ACT> fifo_arr[POY-1][POX];
+    #pragma HLS STREAM variable=fifo_arr depth=FIFO_ARR_DEPTH
+    
+    for (unsigned int cnt = 0; cnt < total_loops; cnt++) {
+        // last poy
+        // copy input buffer content to register
+        unsigned int last_y_idx = cnt / nky;
+        unsigned int last_x_idx = cnt % nkx;
+        if (cnt % nkx == 0) {
+            #pragma HLS unroll
+            for (int x = 0; x < POX+1; x++) {
+                // first row of input buffer
+                buf2pe_reg[POY-1][x] = input_buffer[db_idx][POY-1+last_y_idx][x];
+            }
+        }
+        // last register gets new val. rest values are fed from adjacent reg
+        else if (cnt % nkx < nkx-1) {
+            #pragma HLS unroll
+            for (int x = 0; x < POX; x++) {
+                buf2pe_reg[POY-1][x] = buf2pe_reg[POY-1][x+1];
+            }
+            buf2pe_reg[POY-1][POX] = input_buffer[db_idx][POY-1+last_y_idx][POX+last_x_idx];
+        }
+        // reg values are fed from adjacent reg
+        else if (cnt % nkx == nkx-1) {
+            #pragma HLS unroll
+            for (int x = 0; x < POX; x++) {
+                buf2pe_reg[POY-1][x] = buf2pe_reg[POY-1][x+1];
+            }
+        }
+        if (cnt < nkx*nky-nkx) {
+            // feed into previous fifo
+            #pragma HLS unroll
+            for (int x = 0; x < POX; x++) {
+                fifo_arr[POY-2][x].write(buf2pe_reg[POY-1][x]);
+            }
+        }
+
+        // middle poy
+        #pragma HLS unroll
+        for (int y = 1; y < POY-1; y++) {
+            // copy input buffer content to register at first cycle
+            if (cnt == 0) {
+                #pragma HLS unroll
+                for (int x = 0; x < POX+1; x++) {
+                    // first row of input buffer
+                    buf2pe_reg[y][x] = input_buffer[db_idx][y][x];
+                }
+            }
+            // last register gets new val. rest values are fed from adjacent reg
+            else if (cnt < nkx-1) {
+                #pragma HLS unroll
+                for (int x = 0; x < POX; x++) {
+                    buf2pe_reg[y][x] = buf2pe_reg[y][x+1];
+                }
+                buf2pe_reg[y][POX] = input_buffer[db_idx][y][POX+cnt];
+            }
+            // reg values are fed from adjacent reg
+            else if (cnt == nkx-1) {
+                #pragma HLS unroll
+                for (int x = 0; x < POX; x++) {
+                    buf2pe_reg[y][x] = buf2pe_reg[y][x+1];
+                }
+            }
+            // get values from FIFO
+            if (cnt >= nkx) {
+                #pragma HLS unroll
+                for (int x = 0; x < POX; x++) {
+                    buf2pe_reg[y][x] = fifo_arr[y][x].read();
+                    //std::cout << "read x: " << x << ", y: " << y << std::endl;
+                }
+            }
+            // feed into previous fifo
+            if (cnt < nkx*nky-nkx) {
+                #pragma HLS unroll
+                for (int x = 0; x < POX; x++) {
+                    fifo_arr[y-1][x].write(buf2pe_reg[y][x]);
+                    //std::cout << "write x: " << x << ", y: " << y << std::endl;
+                }
+            }
+        }
+
+        // first poy
+        // copy input buffer content to register at first cycle
+        if (cnt == 0) {
+            #pragma HLS unroll
+            for (int x = 0; x < POX+1; x++) {
+                // first row of input buffer
+                buf2pe_reg[0][x] = input_buffer[db_idx][0][x];
+            }
+        }
+        // last register gets new val. rest values are fed from adjacent reg
+        else if (cnt < nkx-1) {
+            #pragma HLS unroll
+            for (int x = 0; x < POX; x++) {
+                buf2pe_reg[0][x] = buf2pe_reg[0][x+1];
+            }
+            buf2pe_reg[0][POX] = input_buffer[db_idx][0][POX+cnt];
+        }
+        // reg values are fed from adjacent reg
+        else if (cnt == nkx-1) {
+            #pragma HLS unroll
+            for (int x = 0; x < POX; x++) {
+                buf2pe_reg[0][x] = buf2pe_reg[0][x+1];
+            }
+        }
+        // get values from FIFO
+        if (cnt >= nkx) {
+            #pragma HLS unroll
+            for (int x = 0; x < POX; x++) {
+                buf2pe_reg[0][x] = fifo_arr[0][x].read();
+            }
+        }
+
+        // feed mac unit
+        #pragma HLS unroll
+        for (int y = 0; y < POY; y++) {
+            #pragma HLS unroll
+            for (int x = 0; x < POX; x++) {
+                mac_in_fifo_arr[y][x].write(buf2pe_reg[y][x]);
+            }
+        }
+    }
+}
+
+// BUF2PE
 void BUF2PE_stride(
     // DTYPE_ACT *input_buffer, 
     DTYPE_ACT input_buffer_stride[2][POY*MAX_STRIDE+PAD*2][POX*MAX_STRIDE+PAD*2],
@@ -166,6 +305,33 @@ void BUF2PE_stride(
     }
 }
 
+
+void load_input_buffer(
+    DTYPE_ACT input_buffer[2][POY+PAD*2][POX+PAD*2],
+    DTYPE_MEM act_mem[ACT_MEM_SIZE],
+    unsigned int act_fidx,
+    unsigned int act_yidx,
+    unsigned int act_xidx,
+    unsigned int db_idx     // double buffering index) 
+){
+    unsigned int act_mem_base_idx = act_fidx*NIY*NIX + act_yidx*NIX + act_xidx;
+    for (int y = 0; y < POY+PAD*2; y++) {
+        for (int x = 0; x < POX+PAD*2; x++) {
+            if ( (act_yidx + y < PAD) || (act_yidx + y >= NIY + PAD) || (act_xidx + x < PAD) || (act_xidx + x >= NIX + PAD)) {
+                input_buffer[db_idx][y][x] = 0;
+            }
+            else {
+                unsigned int act_mem_idx = act_mem_base_idx + y * NIX + x;
+                unsigned int idx1 = act_mem_idx / MEM_PACK;
+                unsigned int idx2 = act_mem_idx % MEM_PACK;
+                DTYPE_MEM block = act_mem[idx1];
+                DTYPE_ACT data;
+                data.range() = block.range(W_ACT*(idx2+1)-1,W_ACT*idx2);
+                input_buffer[db_idx][y][x] = data;
+            }
+        }
+    }
+}
 
 void load_input_buffer_stride(
     DTYPE_ACT input_buffer_stride[2][POY*MAX_STRIDE+PAD*2][POX*MAX_STRIDE+PAD*2],
