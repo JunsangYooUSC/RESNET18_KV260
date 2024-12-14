c704ebfdaf089c8310bdfd4cdda8a545bdb861ad
JunsangYooUSC <junsang@usc.edu>
2024-12-14 14:51:26 -0800

    commit

--- src2/kernel_func.hpp
+++ src2/kernel_func.hpp
@@ -135,13 +135,18 @@ void BUF2PE_stride(
                         for (int x = 0; x < POX*s+pad*2; x++) {
                             // zero padding cond
                             bool zero_pad_cond = (y0 + y < pad) || (y0 + y >= noy*s + pad) || (x0 + x < pad) || (x0 + x >= nox*s + pad);
-                            unsigned int mem_idx = act_mem_base_idx + (y-pad) * nox*s + (x-pad);
-                            unsigned int idx1 = mem_idx / MEM_PACK;
-                            unsigned int idx2 = mem_idx % MEM_PACK;
-                            DTYPE_MEM block = act_mem[0][idx1];
                             DTYPE_ACT data;
-                            data.range() = block.range(W_ACT*(idx2+1)-1,W_ACT*idx2);
-                            input_buffer_stride[0][y][x] = (zero_pad_cond) ? (DTYPE_ACT) 0 : data;
+                            if (zero_pad_cond) {
+                                data = 0;
+                            } 
+                            else {
+                                unsigned int mem_idx = act_mem_base_idx + (y-pad) * nox*s + (x-pad);
+                                unsigned int idx1 = mem_idx / MEM_PACK;
+                                unsigned int idx2 = mem_idx % MEM_PACK;
+                                DTYPE_MEM block = act_mem[0][idx1];
+                                data.range() = block.range(W_ACT*(idx2+1)-1,W_ACT*idx2);
+                            }
+                            input_buffer_stride[0][y][x] = data;
                         }
                     }
                     // intra input tile
