2f27216b8c094b84a7fc8a29967e0dcc9a1be411
JunsangYooUSC <junsang@usc.edu>
2024-12-15 11:37:25 -0800

    commit

--- src/host.cpp
+++ src/host.cpp
@@ -119,50 +119,53 @@ int main(){
 	// load weights
 	const std::string fname = "/home/junsang/projects/EE511/hw4/RESNET18_KV260/src/data/layer4_0_conv1_weights.bin";
 	read_bin_fixed<DTYPE_FIL>(fname, weight_mem, BB7_CONV1_CONV_WEIGHT_SIZE);
-	std::cout << "weight_mem[0]: " << weight_mem[0] << std::endl;
-	std::cout << "weight_mem[1]: " << weight_mem[1] << std::endl;
+	const std::string fname = "/home/junsang/projects/EE511/hw4/RESNET18_KV260/src/data/layer4_0_conv2_weights.bin";
+	read_bin_fixed<DTYPE_FIL>(fname, BB7_CONV2_WEIGHT_BASE+weight_mem, BB7_CONV2_CONV_WEIGHT_SIZE);
 	
-	// read_bin_fixed<DTYPE_FIL>(fname, weight_mem, BB7_CONV1_CONV_WEIGHT_SIZE);
-	gen_rand<DTYPE_FIL, BB7_CONV1_CONV_WEIGHT_SIZE>(weight_mem+BB7_CONV1_WEIGHT_BASE, -1, 1);
-	gen_rand<DTYPE_FIL, BB7_CONV2_CONV_WEIGHT_SIZE>(weight_mem+BB7_CONV2_WEIGHT_BASE, -1, 1);
-	gen_rand<DTYPE_FIL, BB7_SKIP_CONV_WEIGHT_SIZE>(weight_mem+BB7_SKIP_WEIGHT_BASE, -1, 1);
-	gen_rand<float, BB7_CONV1_BN_WEIGHT_SIZE>(bn_weight_mem+BB7_CONV1_BN_WEIGHT_BASE, -0.5,0.5);
-	gen_rand<float, BB7_CONV2_BN_WEIGHT_SIZE>(bn_weight_mem+BB7_CONV2_BN_WEIGHT_BASE, -0.5,0.5);
-	gen_rand<float, BB7_SKIP_BN_WEIGHT_SIZE>(bn_weight_mem+BB7_SKIP_BN_WEIGHT_BASE, -0.5,0.5);
-	if (BB7_SKIP_WEIGHT_BASE + BB7_SKIP_CONV_WEIGHT_SIZE <= WEIGHT_MEM_SIZE) {
-		std::cout << "it is OK\n";
-	}
-	if (BB7_SKIP_BN_WEIGHT_BASE + BB7_SKIP_BN_WEIGHT_SIZE <= BN_WEIGHT_MEM_SIZE) {
-		std::cout << "it is OK\n";
-	}
-
-	// host-side data
-	DTYPE_ACT in_act_host[BB6_SKIP_C * BB6_SKIP_H * BB6_SKIP_W];
-	// DTYPE_FIL in_fil_host[TOTAL_FIL_LEN];
-	DTYPE_ACT out_act_host[BB7_CONV1_C * BB7_CONV1_H * BB7_CONV1_W];
-	float in_act_host_float[BB6_SKIP_C * BB6_SKIP_H * BB6_SKIP_W];
-	float in_fil_host_float[BB6_SKIP_C * BB7_CONV1_C * BB7_CONV1_H * BB7_CONV1_W];
-	float out_act_host_float[BB7_CONV1_C * BB7_CONV1_H * BB7_CONV1_W];
-	// generate random input activation and filter value with float
-	gen_rand<DTYPE_ACT, BB6_SKIP_C * BB6_SKIP_H * BB6_SKIP_W>(in_act_host, -1, 1);
-	// gen_rand<DTYPE_FIL, TOTAL_FIL_LEN>(in_fil_host, -1, 1);
-	for (int idx = 0; idx < BB6_SKIP_C * BB6_SKIP_H * BB6_SKIP_W; idx++) {
-		in_act_host_float[idx] = in_act_host[idx];
-	}
-	for (int idx = 0; idx < BB6_SKIP_C * BB7_CONV1_C * BB7_CONV1_H * BB7_CONV1_W; idx++) {
-		in_fil_host_float[idx] = weight_mem[idx];
-	}
-
-	// golden convolution result with fixed point and float
-	convolution_golden<DTYPE_ACT, DTYPE_FIL, DTYPE_MUL, DTYPE_MAC>(in_act_host, weight_mem, out_act_host,
-			BB7_CONV1_K, BB7_CONV1_K, BB7_CONV1_C, BB6_SKIP_C, BB7_CONV1_H, BB7_CONV1_W, BB7_CONV1_S, BB7_CONV1_PAD);
-	convolution_golden<float, float, float, float>(in_act_host_float, in_fil_host_float, out_act_host_float,
-			BB7_CONV1_K, BB7_CONV1_K, BB7_CONV1_C, BB6_SKIP_C, BB7_CONV1_H, BB7_CONV1_W, BB7_CONV1_S, BB7_CONV1_PAD);
-
-	//std::cout << "out_act_host[0] = " << out_act_host[0] << std::endl;
-
-	// compare with golden result
-	compare_result<DTYPE_ACT, float, BB7_CONV1_C * BB7_CONV1_H * BB7_CONV1_W>(out_act_host, out_act_host_float, 2.0/(1<<(W_ACT-I_ACT)));
+	std::cout << "weight_mem[BB7_CONV2_WEIGHT_BASE]: " << weight_mem[BB7_CONV2_WEIGHT_BASE] << std::endl;
+	std::cout << "weight_mem[BB7_CONV2_WEIGHT_BASE+1]: " << weight_mem[BB7_CONV2_WEIGHT_BASE+1] << std::endl;
+	
+//	// read_bin_fixed<DTYPE_FIL>(fname, weight_mem, BB7_CONV1_CONV_WEIGHT_SIZE);
+//	gen_rand<DTYPE_FIL, BB7_CONV1_CONV_WEIGHT_SIZE>(weight_mem+BB7_CONV1_WEIGHT_BASE, -1, 1);
+//	gen_rand<DTYPE_FIL, BB7_CONV2_CONV_WEIGHT_SIZE>(weight_mem+BB7_CONV2_WEIGHT_BASE, -1, 1);
+//	gen_rand<DTYPE_FIL, BB7_SKIP_CONV_WEIGHT_SIZE>(weight_mem+BB7_SKIP_WEIGHT_BASE, -1, 1);
+//	gen_rand<float, BB7_CONV1_BN_WEIGHT_SIZE>(bn_weight_mem+BB7_CONV1_BN_WEIGHT_BASE, -0.5,0.5);
+//	gen_rand<float, BB7_CONV2_BN_WEIGHT_SIZE>(bn_weight_mem+BB7_CONV2_BN_WEIGHT_BASE, -0.5,0.5);
+//	gen_rand<float, BB7_SKIP_BN_WEIGHT_SIZE>(bn_weight_mem+BB7_SKIP_BN_WEIGHT_BASE, -0.5,0.5);
+//	if (BB7_SKIP_WEIGHT_BASE + BB7_SKIP_CONV_WEIGHT_SIZE <= WEIGHT_MEM_SIZE) {
+//		std::cout << "it is OK\n";
+//	}
+//	if (BB7_SKIP_BN_WEIGHT_BASE + BB7_SKIP_BN_WEIGHT_SIZE <= BN_WEIGHT_MEM_SIZE) {
+//		std::cout << "it is OK\n";
+//	}
+//
+//	// host-side data
+//	DTYPE_ACT in_act_host[BB6_SKIP_C * BB6_SKIP_H * BB6_SKIP_W];
+//	// DTYPE_FIL in_fil_host[TOTAL_FIL_LEN];
+//	DTYPE_ACT out_act_host[BB7_CONV1_C * BB7_CONV1_H * BB7_CONV1_W];
+//	float in_act_host_float[BB6_SKIP_C * BB6_SKIP_H * BB6_SKIP_W];
+//	float in_fil_host_float[BB6_SKIP_C * BB7_CONV1_C * BB7_CONV1_H * BB7_CONV1_W];
+//	float out_act_host_float[BB7_CONV1_C * BB7_CONV1_H * BB7_CONV1_W];
+//	// generate random input activation and filter value with float
+//	gen_rand<DTYPE_ACT, BB6_SKIP_C * BB6_SKIP_H * BB6_SKIP_W>(in_act_host, -1, 1);
+//	// gen_rand<DTYPE_FIL, TOTAL_FIL_LEN>(in_fil_host, -1, 1);
+//	for (int idx = 0; idx < BB6_SKIP_C * BB6_SKIP_H * BB6_SKIP_W; idx++) {
+//		in_act_host_float[idx] = in_act_host[idx];
+//	}
+//	for (int idx = 0; idx < BB6_SKIP_C * BB7_CONV1_C * BB7_CONV1_H * BB7_CONV1_W; idx++) {
+//		in_fil_host_float[idx] = weight_mem[idx];
+//	}
+//
+//	// golden convolution result with fixed point and float
+//	convolution_golden<DTYPE_ACT, DTYPE_FIL, DTYPE_MUL, DTYPE_MAC>(in_act_host, weight_mem, out_act_host,
+//			BB7_CONV1_K, BB7_CONV1_K, BB7_CONV1_C, BB6_SKIP_C, BB7_CONV1_H, BB7_CONV1_W, BB7_CONV1_S, BB7_CONV1_PAD);
+//	convolution_golden<float, float, float, float>(in_act_host_float, in_fil_host_float, out_act_host_float,
+//			BB7_CONV1_K, BB7_CONV1_K, BB7_CONV1_C, BB6_SKIP_C, BB7_CONV1_H, BB7_CONV1_W, BB7_CONV1_S, BB7_CONV1_PAD);
+//
+//	//std::cout << "out_act_host[0] = " << out_act_host[0] << std::endl;
+//
+//	// compare with golden result
+//	compare_result<DTYPE_ACT, float, BB7_CONV1_C * BB7_CONV1_H * BB7_CONV1_W>(out_act_host, out_act_host_float, 2.0/(1<<(W_ACT-I_ACT)));
 
 	//kernel_func(in_act_host, in_fil_host, out_act_host);
 	//compare_result<DTYPE_ACT, float, TOTAL_OUT_LEN>(out_act_host, out_act_host_float, 2.0/(1<<(W_ACT-I_ACT)));
