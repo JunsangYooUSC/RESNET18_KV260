b4792b468b221a97814e45561c8553914b2b2b7c
JunsangYooUSC <junsang@usc.edu>
2024-12-15 20:53:30 -0800

    commit

--- src/host.cpp
+++ src/host.cpp
@@ -341,26 +341,26 @@ int main(){
 	}
 
 	// golden convolution result with fixed point and float
-// 	convolution_golden<DTYPE_ACT, DTYPE_FIL, DTYPE_MUL, DTYPE_MAC>(in_act_host, weight_mem, out_act_host,
-// 			BB7_CONV1_K, BB7_CONV1_K, BB7_CONV1_C, BB6_SKIP_C, BB7_CONV1_H, BB7_CONV1_W, BB7_CONV1_S, BB7_CONV1_PAD);
-// 	convolution_golden<float, float, float, float>(in_act_host_float, in_fil_host_float, out_act_host_float,
-// 			BB7_CONV1_K, BB7_CONV1_K, BB7_CONV1_C, BB6_SKIP_C, BB7_CONV1_H, BB7_CONV1_W, BB7_CONV1_S, BB7_CONV1_PAD);
-//
-//	//std::cout << "out_act_host[0] = " << out_act_host[0] << std::endl;
-//
-//	// compare with golden result
-//	compare_result<DTYPE_ACT, float, BB7_CONV1_C * BB7_CONV1_H * BB7_CONV1_W>(out_act_host, out_act_host_float, 2.0/(1<<(W_ACT-I_ACT)));
-//
-//	// kernel_func(in_act_host, weight_mem, bn_weight_mem, out_act_host);
-//
-//	kernel_test_func(in_act_host, weight_mem, bn_weight_mem, out_act_host);
-//	compare_result<DTYPE_ACT, float, BB7_CONV1_C * BB7_CONV1_H * BB7_CONV1_W>(out_act_host, out_act_host_float, 2.0/(1<<(W_ACT-I_ACT)));
-//
-//	// conv with bn
-//	convolution_bn_golden<float, float, float, float>(in_act_host_float, in_fil_host_float, out_act_host_float, bn_weight_mem,
-//			BB7_CONV1_K, BB7_CONV1_K, BB7_CONV1_C, BB6_SKIP_C, BB7_CONV1_H, BB7_CONV1_W, BB7_CONV1_S, BB7_CONV1_PAD);
-//	kernel_test2_func(in_act_host, weight_mem, bn_weight_mem, out_act_host);
-//	compare_result<DTYPE_ACT, float, BB7_CONV1_C * BB7_CONV1_H * BB7_CONV1_W>(out_act_host, out_act_host_float, 2.0/(1<<(W_ACT-I_ACT)));
+ 	convolution_golden<DTYPE_ACT, DTYPE_FIL, DTYPE_MUL, DTYPE_MAC>(in_act_host, weight_mem, out_act_host,
+ 			BB7_CONV1_K, BB7_CONV1_K, BB7_CONV1_C, BB6_SKIP_C, BB7_CONV1_H, BB7_CONV1_W, BB7_CONV1_S, BB7_CONV1_PAD);
+ 	convolution_golden<float, float, float, float>(in_act_host_float, in_fil_host_float, out_act_host_float,
+ 			BB7_CONV1_K, BB7_CONV1_K, BB7_CONV1_C, BB6_SKIP_C, BB7_CONV1_H, BB7_CONV1_W, BB7_CONV1_S, BB7_CONV1_PAD);
+
+	//std::cout << "out_act_host[0] = " << out_act_host[0] << std::endl;
+
+	// compare with golden result
+	compare_result<DTYPE_ACT, float, BB7_CONV1_C * BB7_CONV1_H * BB7_CONV1_W>(out_act_host, out_act_host_float, 2.0/(1<<(W_ACT-I_ACT)));
+
+	// kernel_func(in_act_host, weight_mem, bn_weight_mem, out_act_host);
+
+	kernel_test_func(in_act_host, weight_mem, bn_weight_mem, out_act_host);
+	compare_result<DTYPE_ACT, float, BB7_CONV1_C * BB7_CONV1_H * BB7_CONV1_W>(out_act_host, out_act_host_float, 2.0/(1<<(W_ACT-I_ACT)));
+
+	// conv with bn
+	convolution_bn_golden<float, float, float, float>(in_act_host_float, in_fil_host_float, out_act_host_float, bn_weight_mem,
+			BB7_CONV1_K, BB7_CONV1_K, BB7_CONV1_C, BB6_SKIP_C, BB7_CONV1_H, BB7_CONV1_W, BB7_CONV1_S, BB7_CONV1_PAD);
+	kernel_test2_func(in_act_host, weight_mem, bn_weight_mem, out_act_host);
+	compare_result<DTYPE_ACT, float, BB7_CONV1_C * BB7_CONV1_H * BB7_CONV1_W>(out_act_host, out_act_host_float, 2.0/(1<<(W_ACT-I_ACT)));
 
 	// conv, bn, skip conn, relu
 	// convolution_bn_skip_relu_golden<float, float, float, float>(in_act_host_float, in_fil_host_float, out_act_host_float, bn_weight_mem, in_add_host_float,
