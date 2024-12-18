d74023e0df222fcc4f0e3e1ee3f48775f696f6ef
JunsangYooUSC <junsang@usc.edu>
2024-12-18 04:19:46 -0800

    commit

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -289,7 +289,7 @@ void skip_conn(
                         for (int x = 0; x < POX; x++) {
                             float val = in_fifo.read();
                             if (skip_en) {
-                                unsigned add_addr = (f_out+f)*noy*nox + (y0+y)*nox + x0;
+                                unsigned add_addr = (f_out+f)*noy*nox + (y0+y)*nox + x0+x;
                                 std::cout << "add_addr: " << add_addr << std::endl;
                                 DTYPE_ACT add_val = act_mem[base_addr + add_addr];
                                 val = val + (float)add_val;
