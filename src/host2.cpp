83a3086c42f6a71ca0a8f4d933017e082fad3640
JunsangYooUSC <junsang@usc.edu>
2024-12-18 15:51:37 -0800

    commit

--- src/host2.cpp
+++ src/host2.cpp
@@ -211,33 +211,33 @@ int main(){
 		
 		// for debugging
 		std::cout << "layer_cnt: " << layer_cnt << std::endl;
-		std::cout << "nif" << nif << std::endl;
-		std::cout << "nof" << nof << std::endl;
-		std::cout << "noy" << noy << std::endl;
-		std::cout << "nox" << nox << std::endl;
-		std::cout << "nkx" << nkx << std::endl;
-		std::cout << "nky" << nky << std::endl;
-		std::cout << "stride" << stride << std::endl;
-		std::cout << "pad" << pad << std::endl;
-		std::cout << "bb_en" << bb_en << std::endl;
-		std::cout << "conv_en" << conv_en << std::endl;
-		std::cout << "bn_en" << bn_en << std::endl;
-		std::cout << "skip_en" << skip_en << std::endl;
-		std::cout << "relu_en" << relu_en << std::endl;
-		std::cout << "max_pool_en" << max_pool_en << std::endl;
-		std::cout << "avg_pool_en" << avg_pool_en << std::endl;
-		std::cout << "lin_en" << lin_en << std::endl;
-		std::cout << "base_addr_in" << base_addr_in << std::endl;
-		std::cout << "base_addr_out" << base_addr_out << std::endl;
-		std::cout << "base_addr_add" << base_addr_add << std::endl;
-		std::cout << "weight_base" << weight_base << std::endl;
-		std::cout << "weight_size" << weight_size << std::endl;
-		std::cout << "bn_weight_base" << bn_weight_base << std::endl;
-		std::cout << "bn_weight_size" << bn_weight_size << std::endl;
-		std::cout << "in_size" << in_size << std::endl;
-		std::cout << "out_size" << out_size << std::endl;
+		std::cout << "nif: " << nif << std::endl;
+		std::cout << "nof: " << nof << std::endl;
+		std::cout << "noy: " << noy << std::endl;
+		std::cout << "nox: " << nox << std::endl;
+		std::cout << "nkx: " << nkx << std::endl;
+		std::cout << "nky: " << nky << std::endl;
+		std::cout << "stride: " << stride << std::endl;
+		std::cout << "pad: " << pad << std::endl;
+		std::cout << "bb_en: " << bb_en << std::endl;
+		std::cout << "conv_en: " << conv_en << std::endl;
+		std::cout << "bn_en: " << bn_en << std::endl;
+		std::cout << "skip_en: " << skip_en << std::endl;
+		std::cout << "relu_en: " << relu_en << std::endl;
+		std::cout << "max_pool_en: " << max_pool_en << std::endl;
+		std::cout << "avg_pool_en: " << avg_pool_en << std::endl;
+		std::cout << "lin_en: " << lin_en << std::endl;
+		std::cout << "base_addr_in: " << base_addr_in << std::endl;
+		std::cout << "base_addr_out: " << base_addr_out << std::endl;
+		std::cout << "base_addr_add: " << base_addr_add << std::endl;
+		std::cout << "weight_base: " << weight_base << std::endl;
+		std::cout << "weight_size: " << weight_size << std::endl;
+		std::cout << "bn_weight_base: " << bn_weight_base << std::endl;
+		std::cout << "bn_weight_size: " << bn_weight_size << std::endl;
+		std::cout << "in_size: " << in_size << std::endl;
+		std::cout << "out_size: " << out_size << std::endl;
 		
-	}
+	}:
 
 	// kernel calculation
 	conv_kernel(act_in_host, act_out_host, weight_mem, bn_weight_mem, &start_layer, &end_layer);
