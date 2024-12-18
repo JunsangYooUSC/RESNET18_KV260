b34c32fc7e0676417fff0dae33951401f7d0dac0
JunsangYooUSC <junsang@usc.edu>
2024-12-18 07:43:16 -0800

    commit

--- src/host2.cpp
+++ src/host2.cpp
@@ -376,5 +376,7 @@ int main(){
 			BB7_SKIP_K, BB7_SKIP_K, BB7_SKIP_C, BB7_CONV2_C, BB7_SKIP_H, BB7_SKIP_W, BB7_SKIP_S, BB7_SKIP_PAD);
 	
 	compare_result<DTYPE_ACT, float, OUTPUT_SIZE>(act_mem+MEM0_SIZE, act_host_float+MEM0_SIZE, 0.1);
-
+	for (int idx = 0; idx < 10; idx++) {
+		std::cout << "output[" << idx << "]: " << act_host_float[MEM0_SIZE+idx] << std::endl;
+	}
 }
