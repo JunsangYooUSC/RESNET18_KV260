180dd6bd5c03cd8a9f5b42a7e09777187770e47b
JunsangYooUSC <junsang@usc.edu>
2024-12-18 21:18:17 -0800

    commit

--- src/host2.cpp
+++ src/host2.cpp
@@ -117,6 +117,68 @@ int main(){
 	unsigned in_size = 0;
 	unsigned out_size = 0;
 
+	// for debug
+    for (layer_cnt = 0; layer_cnt <= 27; layer_cnt++) {
+        controller (
+            &layer_cnt,
+            &nif,
+            &nof,
+            &noy,
+            &nox,
+            &nkx,
+            &nky,
+            &stride,
+            &pad,
+            &bb_en,
+            &conv_en,
+            &bn_en,
+            &skip_en,
+            &relu_en,
+            &max_pool_en,
+            &avg_pool_en,
+            &fc_en,
+            &base_addr_in,
+            &base_addr_out,
+            &base_addr_add,
+            &weight_base,
+            &weight_size,
+            &bn_weight_base,
+            &bn_weight_size,
+            &in_size,
+            &out_size
+        );
+		// for debugging
+		std::cout << "****************************************" << std::endl;
+		std::cout << "layer_cnt: " << layer_cnt << std::endl;
+		std::cout << "nif: " << nif << std::endl;
+		std::cout << "nof: " << nof << std::endl;
+		std::cout << "noy: " << noy << std::endl;
+		std::cout << "nox: " << nox << std::endl;
+		std::cout << "nkx: " << nkx << std::endl;
+		std::cout << "nky: " << nky << std::endl;
+		std::cout << "stride: " << stride << std::endl;
+		std::cout << "pad: " << pad << std::endl;
+		std::cout << "bb_en: " << bb_en << std::endl;
+		std::cout << "conv_en: " << conv_en << std::endl;
+		std::cout << "bn_en: " << bn_en << std::endl;
+		std::cout << "skip_en: " << skip_en << std::endl;
+		std::cout << "relu_en: " << relu_en << std::endl;
+		std::cout << "max_pool_en: " << max_pool_en << std::endl;
+		std::cout << "avg_pool_en: " << avg_pool_en << std::endl;
+		std::cout << "fc_en: " << fc_en << std::endl;
+		std::cout << "base_addr_in: " << base_addr_in << std::endl;
+		std::cout << "base_addr_out: " << base_addr_out << std::endl;
+		std::cout << "base_addr_add: " << base_addr_add << std::endl;
+		std::cout << "weight_base: " << weight_base << std::endl;
+		std::cout << "weight_size: " << weight_size << std::endl;
+		std::cout << "bn_weight_base: " << bn_weight_base << std::endl;
+		std::cout << "bn_weight_size: " << bn_weight_size << std::endl;
+		std::cout << "in_size: " << in_size << std::endl;
+		std::cout << "out_size: " << out_size << std::endl;
+		std::cout << "****************************************" << std::endl;
+		std::cout << std::endl;
+	}
+
 	// mimic controller
     for (layer_cnt = start_layer; layer_cnt <= end_layer; layer_cnt++) {
         controller (
